cb1558b2f361167ef5eae6c662c66de4b990a662
Sieger <khloponin.kirill@gmail.com>
2016-05-28 21:02:11 +0200

    First commit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,70 @@
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
+//----------//----------//----------//
+struct Map{
+    int size;
+    int array[10][10];
+};
+struct Point{
+    int x;
+    int y;
+};
+
+//----------//----------//----------//
+Map createMap();
+void printMap(Map map);
+//----------//----------//----------//
+void start(Map map);
+//----------//----------//----------//
+
 int main() {
-    cout << "Hello, World!" << endl;
+    srand(time(0));
+
+    Map map = createMap();
+    printMap(map);
+
     return 0;
 }
+
+//----------//----------//----------//
+void printMap(Map map){
+    for (int i = 0; i < map.size; ++i) {
+        for (int j = 0; j < map.size; ++j) {
+            cout << map.array[i][j] << " ";
+        }
+        cout<<"\n";
+    }
+}
+Map createMap(){
+    Map map;
+    map.size = 10;
+    for (int i = 0; i < map.size; ++i) {
+        for (int j = 0; j < map.size; ++j) {
+            map.array[i][j] = 0;
+        }
+    }
+    map.array[0][map.size-1]=1;
+    map.array[map.size-1][0]=2;
+
+    return map;
+}
+//----------//----------//----------//
+void start(Map map){
+    bool end = false;
+
+    while(!end){
+        //gameLogic(map);
+        printMap (map);
+    }
+}
+//----------//----------//----------//
+
+void gameLogic(Map map){
+    Point array1[map.size*map.size];
+    Point array2[map.size*map.size];
+
+}
+//----------//----------//----------//
